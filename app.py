1f5236a4cdfa3c1437513312e005fb867d2162bf
Devangana T S <148966769+Thakkudts@users.noreply.github.com>
2026-09-18 11:03:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 import streamlit as st
 import joblib
+from sklearn.preprocessing import PolynomialFeatures
+poly = PolynomialFeatures(degree=2)
 
 model = joblib.load("polynomial_regression_electricity_ac&fan.pkl")
 
-st.title("⚡ AC Units vs Electric Bill")
+st.title("AC Units vs Electric Bill")
 st.write("Predict Electric Bill using Polynomial Regression")
 
 ac_units = st.number_input(
     "Enter AC Electricity Consumption (Units):",
-    min_value=0.0,
+    min_value=1.0,
     value=100.0,
+    max_value=149.0,
     step=1.0
 )
 
 fan_units = st.number_input(
-  "Enter fan Consumption:",
-  min_value=0.0,
-  value=100.0,
-  step=1.0
+    "Enter Fan Electricity Consumption (Units):",
+    min_value=1.0,
+    value=100.0,
+    max_value=149.0,
+    step=1.0
 )
 
 if st.button("Predict Electric Bill"):
-
-    prediction = model.predict([[ac_units],[fan_units]])
-
+    input_data=[[ac_units],[fan_units]]
+    input_poly=poly.fit_transform(input_data)
+    prediction = model.predict(input_poly)
     st.success(f"Expected Electric Bill: ₹{prediction[0]:.2f}")
